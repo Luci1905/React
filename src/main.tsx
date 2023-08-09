73019786dba9fad36d6f1edb9bcfcad5aa1bc59a
Luciane <lucimlozano@gmail.com>
2023-08-08 21:11:36 -0300

    alteracao-Main.tsx-Style.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client'
 
 //componentes
 import Home from './pages/Home';
-
+import ListaServico from './pages/ListaServicos';
 //estilização global
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home />
+    {/* <Home /> */}
+    <ListaServico />
   </React.StrictMode>,
 )
