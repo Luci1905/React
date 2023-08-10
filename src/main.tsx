a9c82b4a8027e4a99d026e3ff9ad15cb69ba86cc
Luciane <lucimlozano@gmail.com>
2023-08-09 21:51:59 -0300

    componetizacao-Header

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ import ReactDOM from 'react-dom/client'
 //componentes
 import Home from './pages/Home';
 import ListaServico from './pages/ListaServicos';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 //estilização global
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Header />
     <Home />
+    <Footer />
     {/* <ListaServico /> */}
   </React.StrictMode>,
 )
